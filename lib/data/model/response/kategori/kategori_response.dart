caa85ff8e20f66ce0bb721a00c27c0383a9570d4
muhfarrasm <muhammadfarrasm@gmail.com>
2025-07-06 00:43:32 +0700

    ADD -  Property untuk kategori target

--- lib/data/model/response/kategori/kategori_response.dart
+++ lib/data/model/response/kategori/kategori_response.dart
@@ -1,9 +1,13 @@
-class KategoriResponse{
+class KategoriResponse {
   final int id;
   final String namaKategori;
   final String? deskripsi;
   final double? anggaran; // hanya pengeluaran yang punya
   final int userId;
+  final int? jumlahTarget; // hanya target
+  final double? totalTargetDana; // hanya target
+  final double? totalTerkumpul; // hanya target
+  final double? persentasePencapaian; // hanya target
   final DateTime createdAt;
   final DateTime updatedAt;
 
@@ -13,6 +17,10 @@ class KategoriResponse{
     this.deskripsi,
     this.anggaran,
     required this.userId,
+    this.jumlahTarget,
+    this.totalTargetDana,
+    this.totalTerkumpul,
+    this.persentasePencapaian,
     required this.createdAt,
     required this.updatedAt,
   });
@@ -22,8 +30,15 @@ class KategoriResponse{
       id: json['id'],
       namaKategori: json['nama_kategori'],
       deskripsi: json['deskripsi'],
-      anggaran: json['anggaran'] != null ? double.tryParse(json['anggaran'].toString()) : null,
+      anggaran:
+          json['anggaran'] != null
+              ? double.tryParse(json['anggaran'].toString())
+              : null,
       userId: json['user_id'],
+      jumlahTarget: json['jumlah_target'],
+      totalTargetDana: json['total_target_dana'] != null ? double.tryParse(json['total_target_dana'].toString()) : null,
+      totalTerkumpul: json['total_terkumpul'] != null ? double.tryParse(json['total_terkumpul'].toString()) : null,
+      persentasePencapaian: json['persentase_pencapaian'] != null ? double.tryParse(json['persentase_pencapaian'].toString()) : null,
       createdAt: DateTime.parse(json['created_at']),
       updatedAt: DateTime.parse(json['updated_at']),
     );
