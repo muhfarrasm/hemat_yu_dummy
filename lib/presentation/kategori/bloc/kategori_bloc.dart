37d6329b4f423766576a1877c163abdcc5ffc4d8
muhfarrasm <muhammadfarrasm@gmail.com>
2025-07-07 05:28:05 +0700

    UPDATE - event dan bloc untuk menambahkan method update, delete, dan pemilihan jenis kategori

--- lib/presentation/kategori/bloc/kategori_bloc.dart
+++ lib/presentation/kategori/bloc/kategori_bloc.dart
@@ -1,27 +1,72 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hematyu_app_dummy_fix/data/model/response/kategori/kategori_response.dart';
 import 'package:hematyu_app_dummy_fix/data/repository/kategori_repository.dart';
 import 'package:hematyu_app_dummy_fix/presentation/kategori/bloc/kategori_event.dart';
 import 'package:hematyu_app_dummy_fix/presentation/kategori/bloc/kategori_state.dart';
-
+import 'package:hematyu_app_dummy_fix/presentation/kategori/bloc/kategori_type.dart';
 class KategoriBloc extends Bloc<KategoriEvent, KategoriState> {
   final KategoriRepository repository;
 
   KategoriBloc(this.repository) : super(KategoriInitial()) {
-    on<FetchKategoriPemasukan>((event, emit) async {
+    on<FetchKategori>((event, emit) async {
       emit(KategoriLoading());
       try {
-        final kategori = await repository.getKategoriPemasukan();
+        List<KategoriResponse> kategori;
+
+        switch (event.type) {
+          case JenisKategori.pemasukan:
+            kategori = await repository.getKategoriPemasukan();
+            break;
+          case JenisKategori.pengeluaran:
+            kategori = await repository.getKategoriPengeluaran();
+            break;
+          case JenisKategori.target:
+            kategori = await repository.getKategoriTarget();
+            break;
+        }
+
         emit(KategoriLoaded(kategori));
       } catch (e) {
         emit(KategoriError(e.toString()));
       }
     });
 
-    on<FetchKategoriPengeluaran>((event, emit) async {
+    on<AddKategori>((event, emit) async {
       emit(KategoriLoading());
       try {
-        final kategori = await repository.getKategoriPengeluaran();
-        emit(KategoriLoaded(kategori));
+        await repository.createKategori(
+          type: event.type,
+          nama: event.nama,
+          deskripsi: event.deskripsi,
+          anggaran: event.anggaran,
+        );
+        add(FetchKategori(event.type));
+      } catch (e) {
+        emit(KategoriError(e.toString()));
+      }
+    });
+
+    on<UpdateKategori>((event, emit) async {
+      emit(KategoriLoading());
+      try {
+        await repository.updateKategori(
+          type: event.type,
+          id: event.id,
+          nama: event.nama,
+          deskripsi: event.deskripsi,
+          anggaran: event.anggaran,
+        );
+        add(FetchKategori(event.type));
+      } catch (e) {
+        emit(KategoriError(e.toString()));
+      }
+    });
+
+    on<DeleteKategori>((event, emit) async {
+      emit(KategoriLoading());
+      try {
+        await repository.deleteKategori(type: event.type, id: event.id);
+        add(FetchKategori(event.type));
       } catch (e) {
         emit(KategoriError(e.toString()));
       }
--- lib/presentation/kategori/bloc/kategori_event.dart
+++ lib/presentation/kategori/bloc/kategori_event.dart
@@ -1,5 +1,42 @@
+
+import 'package:hematyu_app_dummy_fix/presentation/kategori/bloc/kategori_type.dart';
+
 abstract class KategoriEvent {}
 
-class FetchKategoriPemasukan extends KategoriEvent {}
+class FetchKategori extends KategoriEvent {
+  final JenisKategori type;
 
-class FetchKategoriPengeluaran extends KategoriEvent {}
+  FetchKategori(this.type);
+}
+
+
+class AddKategori extends KategoriEvent {
+  final JenisKategori type;
+  final String nama;
+  final String? deskripsi;
+  final double? anggaran;
+  AddKategori({required this.type, required this.nama, this.deskripsi, this.anggaran});
+}
+
+class UpdateKategori extends KategoriEvent {
+  final JenisKategori type;
+  final int id;
+  final String nama;
+  final String? deskripsi;
+  final double? anggaran;
+
+  UpdateKategori({
+    required this.type,
+    required this.id,
+    required this.nama,
+    this.deskripsi,
+    this.anggaran,
+  });
+}
+
+
+class DeleteKategori extends KategoriEvent {
+  final JenisKategori type;
+  final int id;
+  DeleteKategori({required this.type, required this.id});
+}
